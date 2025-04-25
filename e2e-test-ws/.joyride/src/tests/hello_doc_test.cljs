95839baa85e861f156b83e71fa73c46311bf7cc6
PEZ <pez@pezius.com>
2025-04-25 18:28:42 +0200

    Dynamically look up the commands from the manifest for the e2e test

--- e2e-test-ws/.joyride/src/tests/hello_doc_test.cljs
+++ e2e-test-ws/.joyride/src/tests/hello_doc_test.cljs
@@ -9,16 +9,12 @@
     (p/let [;; Get commands from the extension's manifest
             extension-id "betterthantomorrow.calva-power-tools"
             extension (vscode/extensions.getExtension extension-id)
-            _ (is (some? extension) (str "Extension not found: " extension-id))
             package-json (when extension (.-packageJSON extension))
             contributed-commands (when package-json
                                    (->> (.-contributes package-json)
                                         (.-commands)
                                         (map #(.-command %))
                                         (into #{}))) ; Convert to set
-            _ (is (seq contributed-commands)
-                  "No commands found in package.json contributes section")
-
             ;; Get all currently registered commands
             all-commands (-> (vscode/commands.getCommands true)
                              (p/then js->clj))
@@ -29,5 +25,9 @@
                                (->> contributed-commands
                                     (filter #(not (contains? available-commands-set %)))
                                     (into #{})))]
-
-      (is (empty? missing-commands) (str "Missing registered commands from manifest: " missing-commands)))))
+      (is (some? extension)
+          (str "The extension is found: " extension-id))
+      (is (seq contributed-commands)
+          (str "The manifest contributes commands " contributed-commands))
+      (is (empty? missing-commands)
+          "All commands from the manifest are registered"))))
