e4dc19ad9bb7e4505ba4d37a04f70a0a9c481d32
Timothy Pratley <timothypratley@gmail.com>
2025-04-26 09:53:30 -0700

    fix typo

--- src/calva_power_tools/tool/clay.cljs
+++ src/calva_power_tools/tool/clay.cljs
@@ -6,7 +6,7 @@
         (quote $current-form) "$file" {:ide :calva})))
 
 (def make-toplevel-form-snippet
-  '(do (cloure.core/require '[scicloj.clay.v2.snippets])
+  '(do (clojure.core/require '[scicloj.clay.v2.snippets])
        (scicloj.clay.v2.snippets/make-form-html!
         (quote $top-level-form) "$file" {:ide :calva})))
 
