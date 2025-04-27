f1872f211f7847e2cd6fee2b36ba4f9648c9f7a8
Timothy Pratley <timothypratley@gmail.com>
2025-04-27 15:12:20 -0700

    update commands to match Clay exports exactly

--- src/calva_power_tools/tool/clay.cljs
+++ src/calva_power_tools/tool/clay.cljs
@@ -2,20 +2,20 @@
   (:require
    [calva-power-tools.extension.calva :as calva]))
 
+(defn format-snippet
+  ([clay-fn-name] (format-snippet clay-fn-name nil))
+  ([clay-fn-name form]
+   (str "(do (clojure.core/require '[scicloj.clay.v2.snippets])" \newline
+        "  (scicloj.clay.v2.snippets/" clay-fn-name form "$file" "{:ide :calva}))" \newline)))
+
 (defn activate! []
   ;; Register commands that call Calva's custom REPL command
-  (calva/register-snippet! "clay.topLevelForm"
-                           '(do (clojure.core/require '[scicloj.clay.v2.snippets])
-                                (scicloj.clay.v2.snippets/make-form-html!
-                                 (quote $top-level-form) "$file" {:ide :calva})))
-  (calva/register-snippet! "clay.file"
-                           '(do (clojure.core/require '[scicloj.clay.v2.snippets])
-                                (scicloj.clay.v2.snippets/make-ns-html!
-                                 "$file" {:ide :calva})))
-  (calva/register-snippet! "clay.currentForm"
-                           '(do (clojure.core/require '[scicloj.clay.v2.snippets])
-                                (scicloj.clay.v2.snippets/make-form-html!
-                                 (quote $current-form) "$file" {:ide :calva})))
-  (calva/register-snippet! "clay.watch"
-                           '(do (clojure.core/require '[scicloj.clay.v2.snippets])
-                                (scicloj.clay.v2.snippets/watch! {:ide :calva}))))
+  (calva/register-snippet! "clay.file" (format-snippet "make-ns-html!"))
+  (calva/register-snippet! "clay.fileQuarto" (format-snippet "make-ns-quarto-html!"))
+  (calva/register-snippet! "clay.fileRevealJs" (format-snippet "make-ns-quarto-revealjs!"))
+  (calva/register-snippet! "clay.currentForm" (format-snippet "make-form-html!" "$current-form"))
+  (calva/register-snippet! "clay.currentFormQuarto" (format-snippet "make-form-quarto-html!" "$current-form"))
+  (calva/register-snippet! "clay.topLevelForm" (format-snippet "make-form-html!" "$top-level-form"))
+  (calva/register-snippet! "clay.topLevelFormQuarto" (format-snippet "make-form-quarto-html!" "$top-level-form"))
+  (calva/register-snippet! "clay.browse" (format-snippet "browse!"))
+  (calva/register-snippet! "clay.watch" (format-snippet "watch!")))
