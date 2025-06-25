52e67183edc0824fcb5054b36ae09d45e8845eca
PEZ <pez@pezius.com>
2025-06-25 13:53:35 +0200

    Ask before loading dependency as a side effect

--- src/calva_power_tools/tool/dataspex.cljs
+++ src/calva_power_tools/tool/dataspex.cljs
@@ -45,32 +45,37 @@
   (p/let [is-loaded (check-dataspex-loaded)]
     (if is-loaded
       (maybe-start-server!)
-      (p/let [_ (load-dependency!)
-              _ (calva/evaluateCode+ js/undefined "(clojure.core/require 'dataspex.core)" "user")]
-        (maybe-start-server!)))))
+      (p/let [choice (vscode/window.showInformationMessage "The Dataspex dependency is not loaded in the REPL. Do you want to load it?" "Yes" "No")]
+        (when (= "Yes" choice)
+          (p/let [_ (load-dependency!)
+                  _ (calva/evaluateCode+ js/undefined "(clojure.core/require 'dataspex.core)" "user")]
+            (maybe-start-server!)))))))
 
 (defn- open-in-editor-webview []
   (p/let [port (ensure-dataspex-loaded-and-running!)]
-    (calva/execute-calva-command! "calva.runCustomREPLCommand"
-                                  {:snippet (str "(clojure.core/tagged-literal 'flare/html {:url \"http://localhost:" port "/\" :title \"Dataspex\"})")})))
+    (when port
+      (calva/execute-calva-command! "calva.runCustomREPLCommand"
+                                    {:snippet (str "(clojure.core/tagged-literal 'flare/html {:url \"http://localhost:" port "/\" :title \"Dataspex\"})")}))))
 
 (defn- open-in-panel-webview [!app-state context]
   (p/let [port (ensure-dataspex-loaded-and-running!)]
-    (panel-view/activate! !app-state context port)))
+    (when port
+      (panel-view/activate! !app-state context port))))
 
 (defn- inspect-form [form label-candidate]
-  (p/let [_ (ensure-dataspex-loaded-and-running!)]
-    (-> (vscode/window.showInputBox #js {:title "Dataspex Inspect: Panel item name"
-                                         :ignoreFocusOut true
-                                         :value label-candidate
-                                         :placeHolder label-candidate})
-        (.then (fn [s]
-                 (calva/evaluateCode+ js/undefined
-                                      (str "(dataspex.core/inspect\""
-                                           (if (string/blank? s)
-                                             "CPT inspect"
-                                             s)
-                                           "\" " form " )")))))))
+  (p/let [port (ensure-dataspex-loaded-and-running!)]
+    (when port
+      (-> (vscode/window.showInputBox #js {:title "Dataspex Inspect: Panel item name"
+                                           :ignoreFocusOut true
+                                           :value label-candidate
+                                           :placeHolder label-candidate})
+          (.then (fn [s]
+                   (calva/evaluateCode+ js/undefined
+                                        (str "(dataspex.core/inspect\""
+                                             (if (string/blank? s)
+                                               "CPT inspect"
+                                               s)
+                                             "\" " form " )"))))))))
 
 (defn- inspect-current-form []
   (let [form (second (calva/currentForm))
