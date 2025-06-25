bf1499d446398b07cd48fbf9a9f8c6fceacce9f2
PEZ <pez@pezius.com>
2025-06-25 18:28:07 +0200

    Open inspector automatically when showing browser inspects

--- src/calva_power_tools/tool/dataspex.cljs
+++ src/calva_power_tools/tool/dataspex.cljs
@@ -48,7 +48,7 @@
       (p/let [choice (vscode/window.showInformationMessage "The Dataspex dependency is not loaded in the REPL. Do you want to load it?" "Yes" "No")]
         (when (= "Yes" choice)
           (p/let [_ (load-dependency!)
-                  _ (calva/evaluateCode+ js/undefined "(#?(:clj clojure.core/require :cljs require) 'dataspex.core)" "user")]
+                  _ (calva/evaluateCode+ js/undefined "(#?(:clj clojure.core/require :cljs require) 'dataspex.core)")]
             (maybe-start-server!)))))))
 
 (defn- open-in-editor-webview []
@@ -57,7 +57,7 @@
       (calva/execute-calva-command! "calva.runCustomREPLCommand"
                                     {:snippet (str "(clojure.core/tagged-literal 'flare/html {:url \"http://localhost:" port "/\" :title \"Dataspex\"})")}))))
 
-(defn- open-in-panel-webview [!app-state context]
+(defn- open-panel-webview! [!app-state context]
   (p/let [port (ensure-dataspex-loaded-and-running!)]
     (when port
       (panel-view/activate! !app-state context port))))
@@ -71,7 +71,7 @@
                                            :placeHolder label-candidate})
           (.then (fn [s]
                    (calva/evaluateCode+ js/undefined
-                                        (str "(require 'dataspex.core)\n"
+                                        (str "(#?(:clj clojure.core/require :cljs require) 'dataspex.core)"
                                              "(dataspex.core/inspect\""
                                              (if (string/blank? s)
                                                "CPT inspect"
@@ -88,17 +88,34 @@
         label (get-label-candidate form)]
     (inspect-form form label)))
 
+(defn- connect-remote-inspector [!app-state context]
+  (p/let [port (ensure-dataspex-loaded-and-running!)]
+    (when port
+      (p/let [eval-result (calva/evaluateCode+ js/undefined
+                                               (str "(#?(:clj clojure.core/require :cljs require) 'dataspex.core)"
+                                                    "(if-let [connect-remote-inspector (resolve 'dataspex.core/connect-remote-inspector)]"
+                                                    "  (do (connect-remote-inspector \"http://localhost:" port "\")"
+                                                    "      true)"
+                                                    "  false)"))
+              success? (clojure.edn/read-string (.-result eval-result))]
+        (if success?
+          (open-panel-webview! !app-state context)
+          (throw (ex-info "Failed to connect the remote Dataspex inspector. You need to do this from a connected ClojureScript REPL."
+                          {:port port})))))))
+
 (defn- inspect-remote []
   (p/let [port (ensure-dataspex-loaded-and-running!)]
     (when port
-      (calva/evaluateCode+ js/undefined (str "(if (resolve (quote dataspex.core/inspect-remote))"
-                                             "(dataspex.core/inspect-remote \"http://localhost:" port "\"))")))))
-
-(defn- connect-remote-inspector []
-  (p/let [port (ensure-dataspex-loaded-and-running!)]
-    (when port
-      (calva/evaluateCode+ js/undefined (str "(if (resolve (quote dataspex.core/connect-remote-inspector))"
-                                             "(dataspex.core/connect-remote-inspector \"http://localhost:" port "\"))")))))
+      (p/let [eval-result (calva/evaluateCode+ js/undefined
+                                               (str "(#?(:clj clojure.core/require :cljs require) 'dataspex.core)"
+                                                    "(if-let [inspect-remote (resolve 'dataspex.core/inspect-remote)]"
+                                                    "  (do (inspect-remote \"http://localhost:" port "\")"
+                                                    "      true)"
+                                                    "  false)"))
+              success? (clojure.edn/read-string (.-result eval-result))]
+        (when-not success?
+          (throw (ex-info "Failed inspect to the remote Dataspex inspector."
+                          {:port port})))))))
 
 (defn- register-command! [command f]
   (lc-helpers/register-command! db/!app-db command f))
@@ -108,6 +125,6 @@
   (register-command! "cpt.dataspex.inspectCurrentForm" #'inspect-current-form)
   (register-command! "cpt.dataspex.inspectTopLevelForm" #'inspect-top-level-form)
   (register-command! "cpt.dataspex.openInspectorInEditorView" #'open-in-editor-webview)
-  (register-command! "cpt.dataspex.openInspectorPanelView" (partial #'open-in-panel-webview !app-state context))
-  (register-command! "cpt.dataspex.inspectRemote" (partial #'inspect-remote))
-  (register-command! "cpt.dataspex.connectRemoteInspector" (partial #'connect-remote-inspector)))
+  (register-command! "cpt.dataspex.openInspectorPanelView" (partial #'open-panel-webview! !app-state context))
+  (register-command! "cpt.dataspex.connectRemoteInspector" (partial #'connect-remote-inspector  !app-state context))
+  (register-command! "cpt.dataspex.inspectRemote" #'inspect-remote))
